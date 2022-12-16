e050d883f43e1f0f0e32f8e4adc6a213e2ce3bc7
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-16 10:50:14 +0100

    Fix deploy

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
-    <BrowserRouter>
+    <Router basename="/">
       <App />
-    </BrowserRouter>
+    </Router>
   </AppProvider>
 );
